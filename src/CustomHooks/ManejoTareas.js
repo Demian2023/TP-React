57ab4e79ab3978f4c9d78459272259723a81bed6
Demian <demiandiz@gmail.com>
2023-12-09 13:45:22 -0300

    Cusom hook incorporado

---
+++ src/CustomHooks/ManejoTareas.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid'
+
+
+const ManejoTareas = () => {
+
+    const [tareas, setTareas] = useState(() => {
+        const tareasAlmacenadas = localStorage.getItem('tareasLocalStorage');
+        return tareasAlmacenadas ? JSON.parse(tareasAlmacenadas) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem('tareasLocalStorage', JSON.stringify(tareas));
+    }, [tareas]);
+
+    const agregarTarea = (tarea) => {
+
+        const tareaNueva = {
+            id: uuidv4(),
+            texto: tarea,
+            clase: false,
+        }
+        setTareas([...tareas, tareaNueva]);
+    };
+
+    const toggleClass = (id) => {
+        const cambiarClase = tareas.map(tarea => {
+            if (id === tarea.id) {
+                tarea.clase ? tarea.clase = false : tarea.clase = true
+            }
+            return tarea;
+        });
+        setTareas(cambiarClase);
+    };
+
+    const eliminarTarea = (id) => {
+        const tareasBorradas = tareas.filter(tarea => tarea.id !== id);
+        setTareas(tareasBorradas);
+    }
+
+    return ({ tareas, agregarTarea, toggleClass, eliminarTarea });
+}
+
+export default ManejoTareas;
--- src/componentes/TareasApp.jsx
+++ src/componentes/TareasApp.jsx
@@ -1,45 +1,11 @@
 import FormularioTareas from "./FormularioTareas";
 import ListadoTareas from "./ListadoTareas";
-import { useState, useEffect } from 'react';
-import {v4 as uuidv4} from 'uuid'
+import ManejoTareas from "../CustomHooks/ManejoTareas";
 
 
 const TareasApp = () => {
 
-    const [tareas, setTareas]=useState(() => {
-        const tareasAlmacenadas = localStorage.getItem('tareasLocalStorage');
-        return tareasAlmacenadas ? JSON.parse(tareasAlmacenadas) : [];
-    });
-
-    useEffect(() => {
-        localStorage.setItem('tareasLocalStorage', JSON.stringify(tareas));
-      }, [tareas]);
-
-    const agregarTarea = (tarea) => {
-
-    const tareaNueva = {
-        id: uuidv4(),
-        texto: tarea,
-        clase: false,
-    }
-    setTareas([...tareas, tareaNueva]); 
-    };
-
-    const toggleClass = (id) => {
-        const cambiarClase = tareas.map(tarea =>{
-            if (id === tarea.id) {
-                tarea.clase ? tarea.clase = false : tarea.clase = true
-            }
-            return tarea;
-            });
-            setTareas(cambiarClase);
-    };
-
-    const eliminarTarea = (id) => {
-        const tareasBorradas = tareas.filter(tarea => tarea.id !== id);
-        setTareas(tareasBorradas);
-    }
-
+    const { tareas, agregarTarea, toggleClass, eliminarTarea } = ManejoTareas();
 
     return ( 
         <>
