29f477b5723ec8abfaf52296d9f4322029963ab9
Demian <demiandiz@gmail.com>
2024-04-03 05:44:19 -0300

    mover el drag handle

--- src/componentes/Tarea.jsx
+++ src/componentes/Tarea.jsx
@@ -22,14 +22,18 @@ function Tarea ({ user, toggleClass, abrirModalEditar, eliminarTarea, lista }) {
         <div>
             <li id={user.id} className={user.clase === false ? "noCompletada" : "completada"}>{user.texto}</li>
         </div>
-        <div>
-            <Button variant="outlined" onClick={()=>{toggleClass(lista.titulo, user.id)}} > {user.clase === false ? "Marcar" : "Desmarcar"}</Button >
-            <Button variant="outlined" onClick={()=>{abrirModalEditar(lista.titulo, user.id, user.texto)}} >Editar</Button >
-            <Button variant="outlined" className="eliminar" onClick={()=>{eliminarTarea(lista.titulo, user.id)}} >Eliminar</Button >
-            <Button variant="outlined" {...attributes} {...listeners}>
-                <p style={{margin: "0"}}>Mover título</p> 
-                <svg style={{marginLeft: "5px"}} height="20px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M182.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-96 96c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L128 109.3V402.7L86.6 361.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l96 96c12.5 12.5 32.8 12.5 45.3 0l96-96c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 402.7V109.3l41.4 41.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-96-96z"/></svg>
-            </Button >
+        <div style={{display: "flex", justifyContent: "space-between"}}>
+            <div>
+                <Button variant="outlined" onClick={()=>{toggleClass(lista.titulo, user.id)}} > {user.clase === false ? "Marcar" : "Desmarcar"}</Button >
+                <Button variant="outlined" onClick={()=>{abrirModalEditar(lista.titulo, user.id, user.texto)}} >Editar</Button >
+                <Button variant="outlined" className="eliminar" onClick={()=>{eliminarTarea(lista.titulo, user.id)}} >Eliminar</Button >
+            </div>
+            <div>
+                <Button variant="outlined" {...attributes} {...listeners}>
+                    <p style={{margin: "0"}}>Mover</p> 
+                    <svg style={{marginLeft: "5px"}} height="20px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M182.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-96 96c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L128 109.3V402.7L86.6 361.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l96 96c12.5 12.5 32.8 12.5 45.3 0l96-96c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 402.7V109.3l41.4 41.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-96-96z"/></svg>
+                </Button >
+            </div>
         </div>
     </div>
   )
