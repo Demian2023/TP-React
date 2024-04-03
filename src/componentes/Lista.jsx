02e817564d720be520fe051657605df74f4caa48
Demian <demiandiz@gmail.com>
2024-04-03 06:29:43 -0300

    touch-action none

--- src/componentes/Lista.jsx
+++ src/componentes/Lista.jsx
@@ -32,7 +32,7 @@ function Lista({ user, nombreLista, editar, eliminarTitulo }) {
                     <Button variant="outlined" className="eliminar" onClick={()=>{eliminarTitulo(user.titulo)}}>Eliminar</Button >
                 </div>
                 <div>
-                    <Button variant="outlined" {...attributes} {...listeners}>
+                    <Button variant="outlined" {...attributes} {...listeners} style={{touchAction: "none"}}>
                         <p style={{margin: "0"}}>Mover título</p> 
                         <svg style={{marginLeft: "5px"}} height="20px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M182.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-96 96c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L128 109.3V402.7L86.6 361.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l96 96c12.5 12.5 32.8 12.5 45.3 0l96-96c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 402.7V109.3l41.4 41.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-96-96z"/></svg>
                     </Button >
